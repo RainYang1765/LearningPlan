243f0b6529e6416f83aa0b74632ae941241a3c6a
RainYang1765 <muhua1765@gmail.com>
2026-06-02 20:29:49 +0800

    更新教案

--- lessons/20260531_tree.py
+++ lessons/20260531_tree.py
@@ -4,10 +4,11 @@ class TreeNode:
         self.left = None
         self.right = None
 
-def Str2Tree(istr:str)->TreeNode:
-    #pattern as {1,#,2,3}
-    #2^n - 1 < 节点总数 < 2^(n+1) - 1
-    node_list = istr[1:-1].split(',')
+
+def Str2Tree(istr: str) -> TreeNode:
+    # pattern as {1,#,2,3}
+    # 2^n - 1 < 节点总数 < 2^(n+1) - 1
+    node_list = istr[1:-1].split(",")
     if not node_list:
         return None
     root = TreeNode(int(node_list[0]))
@@ -16,46 +17,71 @@ def Str2Tree(istr:str)->TreeNode:
     while queue and i < len(node_list):
         node = queue.pop(0)
         if node:
-            if node_list[i] != '#':
+            if node_list[i] != "#":
                 node.left = TreeNode(int(node_list[i]))
                 queue.append(node.left)
             i += 1
-            if i < len(node_list) and node_list[i] != '#':
+            if i < len(node_list) and node_list[i] != "#":
                 node.right = TreeNode(int(node_list[i]))
                 queue.append(node.right)
             i += 1
     return root
 
-def PrintTree(root:TreeNode):
+
+def PrintTree(root: TreeNode):
     if not root:
-        print('#', end=' ')
+        print("#", end=" ")
         return
-    print(root.val, end=' ')
+    print(root.val, end=" ")
     PrintTree(root.left)
     PrintTree(root.right)
 
-def BFSTree(root:TreeNode):
+
+def BFSTree(root: TreeNode):
     if not root:
         return
     queue = [root]
     while queue:
         node = queue.pop(0)
         if node:
-            print(node.val, end=' ')
+            print(node.val, end=" ")
             queue.append(node.left)
             queue.append(node.right)
         else:
-            print('#', end=' ')
+            print("#", end=" ")
 
-def DFSTree(root:TreeNode):
-    if not root:
-        print('#', end=' ')
-        return
-    print(root.val, end=' ')
-    DFSTree(root.left)
-    DFSTree(root.right)
 
-if __name__ == '__main__':
-    tree_str = '{1,2,3,4,#,6,7,#,9}'
+def DFSTreePreorder(root: TreeNode):
+    if root:
+        print(root.val, end=" ")
+        DFSTreePreorder(root.left)
+        DFSTreePreorder(root.right)
+    else:
+        print("#", end=" ")
+    return
+
+
+def DFSTreeInorder(root: TreeNode):
+    if root:
+        DFSTreeInorder(root.left)
+        print(root.val, end=" ")
+        DFSTreeInorder(root.right)
+    else:
+        print("#", end=" ")
+    return
+
+
+def DFSTreePostorder(root: TreeNode):
+    if root:
+        DFSTreePostorder(root.left)
+        DFSTreePostorder(root.right)
+        print(root.val, end=" ")
+    else:
+        print("#", end=" ")
+    return
+
+
+if __name__ == "__main__":
+    tree_str = "{1,2,3,4,#,6,7,#,9}"
     root = Str2Tree(tree_str)
     BFSTree(root)
