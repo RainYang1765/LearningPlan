4e3d38b2d2ae1336c596d6d9fb6a3413057919e8
RainYang1765 <muhua1765@gmail.com>
2026-06-04 11:50:13 +0800

    课件更新，堆

--- lessons/20260531_tree.py
+++ lessons/20260531_tree.py
@@ -80,6 +80,33 @@ def DFSTreePostorder(root: TreeNode):
         print("#", end=" ")
     return
 
+def MakeMiniheap(root:TreeNode) -> TreeNode:
+    if not root:
+        return None
+    left_min = MakeMiniheap(root.left)
+    right_min = MakeMiniheap(root.right)
+    if left_min and left_min.val < root.val:
+        root.val, left_min.val = left_min.val, root.val
+    if right_min and right_min.val < root.val:
+        root.val, right_min.val = right_min.val, root.val
+    return root
+
+def TopK(a:list[int], k:int) -> list[int]:
+    # 维护一个大小为k的最小堆
+    import heapq
+    if k <= 0:
+        return []
+    if k > len(a):
+        return sorted(a, reverse=True)
+    min_heap = []
+    for num in a:
+        if len(min_heap) < k:
+            heapq.heappush(min_heap, num)
+        else:
+            if num > min_heap[0]:
+                heapq.heapreplace(min_heap, num)
+    return sorted(min_heap, reverse=True)
+
 
 if __name__ == "__main__":
     tree_str = "{1,2,3,4,#,6,7,#,9}"
